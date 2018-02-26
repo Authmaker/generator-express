5baf0a1cabe5418dcdddbd877bd01fa1e337a344
Chris Manson <chris@stonecircle.io>
2018-02-26 13:16:53 +0000

    removing express-session from the generator

--- generators/app/templates/app/server/index.js
+++ generators/app/templates/app/server/index.js
@@ -1,8 +1,6 @@
 const autoroute = require('express-autoroute');
 const bodyParser = require('body-parser');
-const expressSession = require('express-session');
 const morgan = require('morgan');
-const nconf = require('nconf');
 const path = require('path');
 const winston = require('winston');
 
@@ -11,12 +9,6 @@ module.exports = function initialiseServer(app) {
 
   app.use(bodyParser.urlencoded());
   app.use(bodyParser.json({ type: 'application/vnd.api+json' }));
-
-  app.use(expressSession({
-    secret: nconf.get('server:session_secret'),
-    resave: false,
-    saveUninitialized: true,
-  }));
 
   autoroute(app, {
     routesDir: path.join(__dirname, '/routes'),
